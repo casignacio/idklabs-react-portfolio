a39dc412ad06105fedaa9a6b4cb0189c30e0a1f0
casignacio <carloanthonyignacio@gmail.com>
2023-09-12 12:21:26 -0600

    contact form and footer added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5ea5sxm', 'template_3icmumi', form.current, 'Q9r8d8DXQ6J_SQJao')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  };
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>E-Mail</h4>
+            <h5>carloanthonyignacio@gmail.com</h5>
+            <a href="mailto:carloanthonyignacio@gmail.com">Send A Message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,29 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>idk.labs</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact me</a></li>
+      </ul>
+
+      {/* <div className="footer__socials">
+        <a href="https://indeed.com"></a>
+        <a href="https://github.com"></a>
+        <a href="https://instagram.com"></a>
+        <a href="https://twitter.com"></a>
+      </div> */}
+
+      <div className="footer__copyright">
+        <small>&copy; idk.labs | All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
